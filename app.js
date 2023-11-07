6b9d144deb522e55072300d6094661d49664b455
gata96 <skswoddl16@gmail.com>
2023-11-08 00:14:19 +0900

    Updated #2.2 Variables

---
+++ app.js
@@ -0,0 +1,10 @@
+// JavaScript에서 variable(변수) 만들기
+const a = 5;
+const b = 2;
+
+const myName = "Gata";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello" + myName);
