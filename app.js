c85967b1ef636a7a49df1fb6118fef84e564e74a
gata96 <skswoddl16@gmail.com>
2023-11-08 02:29:23 +0900

    const 대신 let 사용

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // JavaScript에서 variable(변수) 만들기
 const a = 5;
 const b = 2;
-
-const myName = "Gata";
+// const myName = "Gata";
+let myName = "Gata"
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("Hello" + myName);
+console.log("hello " + myName);
+
+myName = "gatinha";
+
+console.log("My new name is " + myName);
