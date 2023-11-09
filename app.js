f4d6c5f91ec363b1fb714b135e7aff175b8f40d1
gata96 <skswoddl16@gmail.com>
2023-11-09 22:52:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// #2.0-2.3
 // JavaScript에서 variable(변수) 만들기
 const a = 5;
 const b = 2;
@@ -12,3 +13,8 @@ console.log("hello " + myName);
 myName = "gatinha";
 
 console.log("My new name is " + myName);
+
+//#2.4 Booleans
+
+const amIFat = true;
+console.log(amIFat);
