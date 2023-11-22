bcb90ca6c42857274de4671c80d7d4dce2ef3a94
gata96 <skswoddl16@gmail.com>
2023-11-22 21:23:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1"); //element 선택
 
-function handleTitleClick() { //함수 실행버튼
-    h1.style.color = "blue"; //실행 결과
-}
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() { 
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI!");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD~")
+function handleTitleClick() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener("click",handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
