cc2a757083ccabd7674dfe6623e7065f92c98568
gata96 <skswoddl16@gmail.com>
2023-11-23 10:40:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1"); //element 선택
 
+// function handleTitleClick() {
+//     if(h1.style.color === "blue") {
+//         h1.style.color = "tomato";
+//     } else {
+//         h1.style.color = "blue";
+//     } 
+// }
+
+// h1.style.color를 변수에 담기
+
 function handleTitleClick() {
     const currentColor = h1.style.color;
     let newColor;
@@ -11,4 +21,4 @@ function handleTitleClick() {
     h1.style.color = newColor;
 }
 
-h1.addEventListener("click",handleTitleClick);
+h1.addEventListener("click", handleTitleClick);
