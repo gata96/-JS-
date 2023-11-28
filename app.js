c5eae19d5f90ee01254982c676ad6fa98f4be6b7
gata96 <skswoddl16@gmail.com>
2023-11-28 23:44:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-// const link = documnet.querySelector("a");
+const link = document.querySelector("a");
 
-// function onLoginSubmit(event) {
-//     event.preventDefault();
-//     console.log(loginInput.value);
-// }
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+}
 
-// function handleLinkClick(event) {
-//     event.preventDefault();
-//     console.dir(event);
-// }
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
 
-// loginForm.addEventListener("submit", onLoginSubmit);
-// link.addEventListener("click", handleLinkClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
 
-console.log(score);
-
-score = 80;
-var score;
-
-console.log(score);
